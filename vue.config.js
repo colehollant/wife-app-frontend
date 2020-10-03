72c372665c33b66c5f6c5a700cb668c466446bbf
Cole Hollant <coleghollant@gmail.com>
2020-10-03 00:53:29 -0400

    try it

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: 'Wife App',
+    themeColor: '#ed70a8',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+  }
+}
